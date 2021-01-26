f70482a851d7bc376cbca26a56b96a9c8a3bd922
razordb9 <thomas.zaussnig@outlook.com>
2021-01-26 14:27:09 +0100

    configured login, added session

--- application/models.py
+++ application/models.py
@@ -1,7 +1,22 @@
 import flask
 from application import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class work(db.Document):
     work_id     =   db.StringField( max_length=10, unique=True )
     title       =   db.StringField( max_length=100 )
     description =   db.StringField( max_length=255 )
+
+class User(db.Document):
+    user_id     =   db.IntField( unique=True)
+    first_name  =   db.StringField( max_length=50)
+    last_name   =   db.StringField( max_length=50)
+    description =   db.StringField( max_length=50)
+    email       =   db.StringField( max_length=100)
+    password    =   db.StringField()
+
+    def set_password(self,password):
+        self.password = generate_password_hash(password)
+    
+    def get_password(self,password):
+        return check_password_hash(self.password, password)
--- application/routes.py
+++ application/routes.py
@@ -1,27 +1,65 @@
 from application import app
-from flask import render_template, redirect, flash, request, Response, json
-from application.forms import LoginForm
-from application.models import work
+from flask import render_template, redirect, flash, request, Response, json, session, url_for
+from application.forms import LoginForm, RegisterForm
+from application.models import work, User
 
 @app.route("/")
 @app.route("/index")
 def index():
     workData = work.objects.all()
-    print(workData)
     return render_template("index.html", index=True, workData=workData)
+
+@app.route("/user_settings")
+def user_settings():
+    userData = User.objects.all()
+    return render_template("user_settings.html", userData=userData)
 
 @app.route("/about")
 def about():
     return render_template("about.html", about=True)
 
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if session.get('username'):
+        return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit() == True:
-        if request.form.get("email") == "thomas.zaussnig@outlook.com":
-            flash("You are successfully logged in!")
+    if form.validate_on_submit():
+        email       = form.email.data
+        password    = form.password.data
+
+        user = User.objects(email=email).first()
+        if user and user.get_password(password):
+            flash("You are successfully logged in!", "success")
+            session['user_id'] = user.user_id
+            session['username'] = user.first_name
             return redirect("/index")
         else:
-            flash("Sorry, something went wrong")
+            flash("Sorry, something went wrong", "danger")
     return render_template("login.html", form=form, login=True, title='Login')
+
+@app.route("/logout")
+def logout():
+    session['user_id']=False
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if session.get('username'):
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_id     = User.objects.count()
+        user_id     += 1
+
+        email       = form.email.data
+        password    = form.password.data
+        first_name  = form.first_name.data
+        last_name   = form.last_name.data
+
+        user = User(user_id=user_id, email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+        flash("You are successfully registered!","success")
+        return redirect(url_for('index'))
+    return render_template("register.html", title="Register", form=form, register=True)
