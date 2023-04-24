fab4d60baa294b4cf2e55e7f3f6bec9d91acf5a5
Thomas Hudson Zaussnig <thomas.zaussnig@outlook.com>
2023-04-24 21:54:50 +0200

    Header, Footer, Impressum

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
-    import '$lib/css/custom_style.css';
-    import '$lib/css/pico.min.css';
-    
+  import "$lib/css/custom_style.css";
+  import "$lib/css/pico.min.css";
 </script>
 
-<slot></slot>
+<html data-theme="dark" />
+<header />
+<main class="container">
+  <nav>
+    <ul>
+      <li><strong>Thomas Hudson Zaussnig</strong></li>
+    </ul>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="#">About</a></li>
+      <!-- <li><a href="#" role="button">Button</a></li> -->
+    </ul>
+  </nav>
+
+  <slot />
+</main>
+
+<footer>
+    <small>
+        Copyright &copy; Thomas HUDSON-ZAUSSNIG •
+        <a href="/impressum">Impressum</a>
+        <a href="/kontakt">Kontakt</a>
+        <a href="https://github.com/razordb9" class="fa-brands fa-github">Github</a>
+    </small>
+</footer>
---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,32 @@
+<main class="container">
+    <h1>Impressum</h1>
+    <p><strong>Angaben gemäß § 5 TMG</strong></p>
+    
+    <span>
+        Ing. Thomas HUDSON-ZAUSSNIG<br>
+        Panorama Straße 50<br>
+        9062 Moosburg<br>
+        <br>
+    </span>
+    
+    <p>
+        <strong>Kontakt</strong><br>
+        Telefon: +43 681 105 99 261 <br>
+        E-Mail: office@thz.at
+    </p>
+    <p>
+        <strong>Redaktionell verantwortlich</strong>
+        <br>
+        Ing. Thomas HUDSON-ZAUSSNIG<br>
+        Panorama Straße 50<br>
+        9062 Moosburg<br>
+    </p>
+    
+    <p>
+        Nach Kleingewerberegelung §19 (I) UStG wird keine Umsatzsteuer ausgewiesen.
+        <br>
+        Trotz sorgfältiger inhaltlicher Kontrolle wird keine Haftung für die Inhalte externer Links übernommen. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich. 
+        <br>
+        <br>
+    </p>
+</main>
