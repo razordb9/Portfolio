51b5781af060f1f57655694e7b2e0febb9ed456b
Thomas Hudson Zaussnig <thomas.zaussnig@outlook.com>
2023-04-24 20:42:27 +0200

    Remove old portofolio page, make new svelte page

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
-
-    MONGODB_SETTINGS = { 'db' : 'PortFolio',
-        'host' : 'mongodb://localhost:27017/PortFolio'
-    }
