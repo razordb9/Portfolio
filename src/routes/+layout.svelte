d69ee8b2ce842f893720d4b7cab226938951a172
Thomas Zaußnig <thomas.zaussnig@outlook.com>
2023-05-12 13:56:21 +0200

    Updated footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,6 +28,13 @@
         Copyright &copy; Thomas HUDSON-ZAUSSNIG •
         <a href="/impressum">Impressum</a>
         <a href="/kontakt">Kontakt</a>
-        <a href="https://github.com/razordb9" class="fa-brands fa-github">Github</a>
+        <a href="https://github.com/razordb9" class="fa-brands fa-github"></a>
+        |
+        <a href="https://www.linkedin.com/in/thomas-hudson-zau%C3%9Fnig-76285a137/" class="fa-brands fa-linkedin"></a>
+        |
+        <a href="https://www.instagram.com/tsonga_64/" class="fa-brands fa-instagram"></a>
+        |
+        <a href="https://twitter.com/ThomasZaussnig" class="fa-brands fa-twitter"></a>
+
     </small>
 </footer>
