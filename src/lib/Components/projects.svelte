c4ed42552167954685f0e4ef55ae77d03a7b92d6
Thomas Zaußnig <44778289+razordb9@users.noreply.github.com>
2023-12-04 17:21:41 +0100

    responsiveness for projects

--- src/lib/Components/projects.svelte
+++ src/lib/Components/projects.svelte
@@ -4,17 +4,23 @@
 </script>
 <div class="project">
 
-    <div class="article">
-        <h2>First project</h2>
-        <footerArticle>Created May, 2023</footerArticle>
-        <div class="articleText">
-            <div class="articleTextPic">
-                <img src="/Logo.png" alt="Hudson-Zaußnig Solutions">
-                
-            </div>
-            <div class="articleTextText">
+    <div class="project-container">
+        
+        <div class="project-card">
+            <h2>First project</h2>
+            <footerArticle>Created May, 2023</footerArticle>
+            <img src="/Logo.png" alt="Hudson-Zaußnig Solutions">
+            <p>
                 Hi, this is my first article on this page on how I started building this portfolio. I will show you all my steps it took me to the point where I am today. I started with the basics of HTML and CSS
-            </div>
+            </p>
+        </div>
+        <div class="project-card">
+            <h2>First project</h2>
+            <footerArticle>Created May, 2023</footerArticle>
+            <img src="/Logo.png" alt="Hudson-Zaußnig Solutions">
+            <p>
+                Hi, this is my first article on this page on how I started building this portfolio. I will show you all my steps it took me to the point where I am today. I started with the basics of HTML and CSS
+            </p>
         </div>
     </div>  
 </div>
