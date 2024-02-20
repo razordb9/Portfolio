41b411c3d0c660d3ce2b275b50baac035f06948d
Thomas Zaußnig <44778289+razordb9@users.noreply.github.com>
2024-02-20 22:09:54 +0100

    create blog page + first basic design

--- src/lib/Components/blog.svelte
+++ src/lib/Components/blog.svelte
@@ -1,8 +1,62 @@
 <script lang="ts">
-    import Blog from '$lib/Components/blog.svelte';
 </script>
-<h1 id="Page_heading">Projects</h1>
-<div class="Page_text">
-    <p>Here you can find all the projects which I already finished or which are in progress</p>
+<div class="blog-container">
+    <div class="blog-item">
+        <img src="/Logo.png" alt="Hudson-Zaußnig Solutions">
+        <a href="#">This is my journey how I started with web design</a>
+        <div>
+            <img class="blog-item-creator" src="/Thomas.jpg" alt="Thomas Hudson-Zaußnig">
+            <span>By</span>
+            Thomas HZ
+            <span>1 hour ago</span>
+        </div>
+    </div>
+    <!-- <div class="blog-item">
+        <h2>First Blog entry</h2>
+        <div>Created February, 2024</div>
+        <p>
+            I will take you on my journey how I started learning webdesign, created
+            my portfolio page and how I try to hustle as a web designer.
+        </p>
+    </div>
+    <div class="blog-item">
+        <h2>First Blog entry</h2>
+        <div>Created February, 2024</div>
+        <p>
+            I will take you on my journey how I started learning webdesign, created
+            my portfolio page and how I try to hustle as a web designer.
+        </p>
+    </div>
+    <div class="blog-item">
+        <h2>First Blog entry</h2>
+        <div>Created February, 2024</div>
+        <p>
+            I will take you on my journey how I started learning webdesign, created
+            my portfolio page and how I try to hustle as a web designer.
+        </p>
+    </div>
+    <div class="blog-item">
+        <h2>First Blog entry</h2>
+        <div>Created February, 2024</div>
+        <p>
+            I will take you on my journey how I started learning webdesign, created
+            my portfolio page and how I try to hustle as a web designer.
+        </p>
+    </div>
+    <div class="blog-item">
+        <h2>First Blog entry</h2>
+        <div>Created February, 2024</div>
+        <p>
+            I will take you on my journey how I started learning webdesign, created
+            my portfolio page and how I try to hustle as a web designer.
+        </p>
+    </div>
+    <div class="blog-item">
+        <h2>First Blog entry</h2>
+        <div>Created February, 2024</div>
+        <p>
+            I will take you on my journey how I started learning webdesign, created
+            my portfolio page and how I try to hustle as a web designer.
+        </p>
+    </div> -->
 </div>
-<Blog>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Blog from '$lib/Components/blog.svelte';
+</script>
+<div class="blog">
+    <h1 class="page-heading">Blog</h1>
+    <div class="page-text">
+        <p>Here you can find all the projects which I already finished or which are in progress</p>
+    </div>
+    <Blog />
+</div>
