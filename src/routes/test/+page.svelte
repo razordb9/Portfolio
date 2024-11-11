3bc586e949cee218f04111a5502ee4442091fb1f
Thomas Zaußnig <44778289+razordb9@users.noreply.github.com>
2024-11-11 22:04:40 +0100

    updated test page with calendly

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,12 +1,19 @@
 <h1 class="page-heading">Test</h1>
 <div class="page-subtext">
-    <p>In this area you can find test stiff I try about.</p>
+    <p>In this area you can find test stuff I try about.</p>
 </div>
-<ul class="flex-list">
+<!-- <ul class="flex-list">
     <li>Item 1</li>
     <li>Item 2</li>
     <li>Item 3</li>
-  </ul>
+  </ul> -->
+
+<!-- Calendly Inline-Widget Beginn -->
+<div class="calendly-inline-widget" data-url="https://calendly.com/thomas-zaussnig/30min" style="min-width:320px;height:700px;"></div>
+<svelte:head>
+  <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+</svelte:head>
+<!-- Calendly Inline-Widget Ende -->
 
 <style>
 .flex-list {
